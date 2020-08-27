64e9e6e19cdb49bce2b970690e82d962ebdfb5ee
AtdheUruqi <atdheuruqidev@gmail.com>
2020-08-27 15:37:08 +0200

    Remove unnecessary files & Started new arch for the app

---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import React, { Fragment, useEffect, useRef, useState } from 'react';
+import './style/styles.css'
+export default () => {
+  return (
+    <div className="header">
+    </div>
+  );
+};
