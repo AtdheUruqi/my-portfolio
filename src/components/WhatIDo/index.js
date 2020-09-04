65ed85c50125fe580c6e7faaf7b6defbfdc18c32
AtdheUruqi <atdheuruqidev@gmail.com>
2020-09-04 18:10:04 +0200

    worked on WhatIDo

--- src/components/WhatIDo/index.js
+++ src/components/WhatIDo/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import './style/styles.css';
 const Skills = () => {
     return (
-        <div style={{backgroundColor:'red',height:'800px'}} >
-           
+        <div style={{ backgroundColor: 'red', height: '800px' }} >
+            <div style={{ backgroundColor: 'yellow', textAlign: 'center' }}>
+                <p>WHAT I DO</p>
+                <p>Things I'm skilled at and passionate about.</p>
+            </div>
+            <div style={{backgroundColor:'violet',height:200}}>
+                 
+            </div>
         </div>
     );
 };
