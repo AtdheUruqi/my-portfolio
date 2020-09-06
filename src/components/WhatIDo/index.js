7ec8d28fa3833d20df10ea3992e481c92c32e30b
AtdheUruqi <atdheuruqidev@gmail.com>
2020-09-06 20:12:33 +0200

    working on bootstrap flex of what i do

--- src/components/WhatIDo/index.js
+++ src/components/WhatIDo/index.js
@@ -7,7 +7,43 @@ const Skills = () => {
                 <p className={'what-i-do'}> WHAT I DO</p>
                 <p>Things I'm skilled at and passionate about.</p>
             </div>
-            <div className={'description-text-div'} >
+            <div class="container" style={{ backgroundColor: 'yellow' }}>
+                <div class="row justify-content-center">
+                    <div class="col-3">
+                        <div style={{ backgroundColor: 'red', width: '100px', height: '100px' }}>
+                            One of three columns
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div style={{ backgroundColor: 'red', width: '100px', height: '100px' }}>
+                            One of three columns
+                        </div>
+                    </div>
+
+                    <div class="col-3">
+                        <div style={{ backgroundColor: 'red', width: '100px', height: '100px' }}>
+                            One of three columns
+                        </div>
+                    </div>
+
+                    <div class="w-100"></div>
+                    <div class="col-sm">
+                        <div style={{ backgroundColor: 'red', width: '100px', height: '100px' }}>
+                            One of three columns
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div style={{ backgroundColor: 'red', width: '100px', height: '100px' }}>
+                            One of three columns
+                        </div>
+                    </div>
+
+                    <div class="col-sm">
+                        <div style={{ backgroundColor: 'red', width: '100px', height: '100px' }}>
+                            One of three columns
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
