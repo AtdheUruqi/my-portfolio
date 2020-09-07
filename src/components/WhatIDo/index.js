4310006bd58e5d6a90e7a2774c0061b932e12cac
AtdheUruqi <atdheuruqidev@gmail.com>
2020-09-07 20:22:26 +0200

    added text for what do i do

--- src/components/WhatIDo/index.js
+++ src/components/WhatIDo/index.js
@@ -9,38 +9,44 @@ const Skills = () => {
             </div>
             <div class="container" style={{ backgroundColor: 'yellow' }}>
                 <div class="row justify-content-center">
-                    <div class="col-3">
-                        <div style={{ backgroundColor: 'red', width: '100px', height: '100px' }}>
-                            One of three columns
+                    <div class="col-sm">
+                        <div style={{ backgroundColor: 'red', }}>
+                            <p>Web Application Development</p>
+                            <p>Fast, responsive and engaging apps that bring your ideas to life.</p>
                         </div>
                     </div>
-                    <div class="col-3">
-                        <div style={{ backgroundColor: 'red', width: '100px', height: '100px' }}>
-                            One of three columns
+                    <div class="col-sm">
+                        <div style={{ backgroundColor: 'red' }}>
+
+                            <p>Mobile Application Development</p>
+                            <p>Cross-platform apps built with efficiency and speed for Android and iOS at once..</p>
                         </div>
                     </div>
 
-                    <div class="col-3">
-                        <div style={{ backgroundColor: 'red', width: '100px', height: '100px' }}>
-                            One of three columns
+                    <div class="col-sm">
+                        <div style={{ backgroundColor: 'red' }}>
+                            <p>API Integration & Development</p>
+                            <p>REST and SOAP API integration. REST API development with Node.js and Loopback.</p>
                         </div>
                     </div>
-
                     <div class="w-100"></div>
                     <div class="col-sm">
-                        <div style={{ backgroundColor: 'red', width: '100px', height: '100px' }}>
-                            One of three columns
+                        <div style={{ backgroundColor: 'red' }}>
+                            <p>Database Design</p>
+                            <p>Database architectures in MongoDB, MySQL or Parse Server, always aiming for performance, scale and stability.</p>
                         </div>
                     </div>
                     <div class="col-sm">
-                        <div style={{ backgroundColor: 'red', width: '100px', height: '100px' }}>
-                            One of three columns
+                        <div style={{ backgroundColor: 'red' }}>
+                            <p>Cloud Integration</p>
+                            <p>Deployment of NodeJS apps, ReactJS apps and databases to leading cloud platforms such as Heroku and Amazon AWS.</p>
                         </div>
                     </div>
 
                     <div class="col-sm">
-                        <div style={{ backgroundColor: 'red', width: '100px', height: '100px' }}>
-                            One of three columns
+                        <div style={{ backgroundColor: 'red' }}>
+                            <p>Payment Gateway Integration</p>
+                            <p>Integration with the most popular Payment Gateways such as Stripe and Paypal.</p>
                         </div>
                     </div>
                 </div>
