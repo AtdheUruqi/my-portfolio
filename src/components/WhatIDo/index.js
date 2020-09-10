63b930e05d8b3982cc8475522f7c02aa9718f269
AtdheUruqi <atdheuruqidev@gmail.com>
2020-09-10 16:43:10 +0200

    remove colors inline

--- src/components/WhatIDo/index.js
+++ src/components/WhatIDo/index.js
@@ -7,52 +7,52 @@ const Skills = () => {
                 <p className={'what-i-do'}> WHAT I DO</p>
                 <p>Things I'm skilled at and passionate about.</p>
             </div>
-            <div class="container" style={{ backgroundColor: 'yellow' }}>
+            <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-sm">
-                        <div style={{ backgroundColor: 'red',textAlign:'center' }}>
+                        <div style={{ textAlign:'center' }}>
                             <p>Web Application Development</p>
                             <img src={require('../../assets/web-development.png')} width="200" height="200"/>
-                            <p style={{backgroundColor:'green',width:'90%',margin:'auto'}}>Fast, responsive and engaging apps that bring your ideas to life.</p>
+                            <p style={{width:'90%',margin:'auto'}}>Fast, responsive and engaging apps that bring your ideas to life.</p>
                         </div>
                     </div>
                     <div class="col-sm">
-                        <div style={{ backgroundColor: 'red',textAlign:'center' }}>
+                        <div style={{ textAlign:'center' }}>
 
                             <p>Mobile Application Development</p>
                             <img src={require('../../assets/mobile-development.png')} width="200" height="200"/>
-                            <p style={{backgroundColor:'green',width:'90%',margin:'auto'}}>Cross-platform apps built with efficiency and speed for Android and iOS at once..</p>
+                            <p style={{width:'90%',margin:'auto'}}>Cross-platform apps built with efficiency and speed for Android and iOS at once..</p>
                         </div>
                     </div>
 
                     <div class="col-sm">
-                        <div style={{ backgroundColor: 'red',textAlign:'center' }}>
+                        <div style={{ textAlign:'center' }}>
                             <p>API Integration & Development</p>
                             <img src={require('../../assets/api-development.png')} width="200" height="200"/>
-                            <p style={{backgroundColor:'green',width:'90%',margin:'auto'}}>REST and SOAP API integration. REST API development with Node.js and Loopback.</p>
+                            <p style={{width:'90%',margin:'auto'}}>REST and SOAP API integration. REST API development with Node.js and Loopback.</p>
                         </div>
                     </div>
                     <div class="w-100"></div>
                     <div class="col-sm">
-                        <div style={{ backgroundColor: 'red',textAlign:'center' }}>
+                        <div style={{ textAlign:'center' }}>
                             <p>Database Design</p>
                             <img src={require('../../assets/database-development.png')} width="200" height="200"/>
-                            <p style={{backgroundColor:'green',width:'90%',margin:'auto'}}>Database architectures in MongoDB, MySQL or Parse Server, always aiming for performance, scale and stability.</p>
+                            <p style={{width:'90%',margin:'auto'}}>Database architectures in MongoDB, MySQL or Parse Server, always aiming for performance, scale and stability.</p>
                         </div>
                     </div>
                     <div class="col-sm">
-                        <div style={{ backgroundColor: 'red',textAlign:'center' }}>
+                        <div style={{ textAlign:'center' }}>
                             <p>Cloud Integration</p>
                             <img src={require('../../assets/cloud-development.png')} width="200" height="200"/>
-                            <p style={{backgroundColor:'green',width:'90%',margin:'auto'}}>Deployment of NodeJS apps, ReactJS apps and databases to leading cloud platforms such as Heroku and Amazon AWS.</p>
+                            <p style={{width:'90%',margin:'auto'}}>Deployment of NodeJS apps, ReactJS apps and databases to leading cloud platforms such as Heroku and Amazon AWS.</p>
                         </div>
                     </div>
 
                     <div class="col-sm">
-                        <div style={{ backgroundColor: 'red',textAlign:'center' }}>
+                        <div style={{ textAlign:'center' }}>
                             <p>Payment Gateway Integration</p>
                             <img src={require('../../assets/payment-development.png')} width="200" height="200"/>
-                            <p style={{backgroundColor:'green',width:'90%',margin:'auto'}}>Integration with the most popular Payment Gateways such as Stripe and Paypal.</p>
+                            <p style={{width:'90%',margin:'auto'}}>Integration with the most popular Payment Gateways such as Stripe and Paypal.</p>
                         </div>
                     </div>
                 </div>
