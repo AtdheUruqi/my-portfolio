e09400ded02a4ed4a1f12e0805fcbe89b91568ab
AtdheUruqi <atdheuruqidev@gmail.com>
2020-09-10 16:34:18 +0200

    worked on what i do image and fixed to center

--- src/components/WhatIDo/index.js
+++ src/components/WhatIDo/index.js
@@ -10,44 +10,49 @@ const Skills = () => {
             <div class="container" style={{ backgroundColor: 'yellow' }}>
                 <div class="row justify-content-center">
                     <div class="col-sm">
-                        <div style={{ backgroundColor: 'red' }}>
+                        <div style={{ backgroundColor: 'red',textAlign:'center' }}>
                             <p>Web Application Development</p>
                             <img src={require('../../assets/web-development.png')} width="200" height="200"/>
-                            <p>Fast, responsive and engaging apps that bring your ideas to life.</p>
+                            <p style={{backgroundColor:'green',width:'90%',margin:'auto'}}>Fast, responsive and engaging apps that bring your ideas to life.</p>
                         </div>
                     </div>
                     <div class="col-sm">
-                        <div style={{ backgroundColor: 'red' }}>
+                        <div style={{ backgroundColor: 'red',textAlign:'center' }}>
 
                             <p>Mobile Application Development</p>
-                            <p>Cross-platform apps built with efficiency and speed for Android and iOS at once..</p>
+                            <img src={require('../../assets/mobile-development.png')} width="200" height="200"/>
+                            <p style={{backgroundColor:'green',width:'90%',margin:'auto'}}>Cross-platform apps built with efficiency and speed for Android and iOS at once..</p>
                         </div>
                     </div>
 
                     <div class="col-sm">
-                        <div style={{ backgroundColor: 'red' }}>
+                        <div style={{ backgroundColor: 'red',textAlign:'center' }}>
                             <p>API Integration & Development</p>
-                            <p>REST and SOAP API integration. REST API development with Node.js and Loopback.</p>
+                            <img src={require('../../assets/api-development.png')} width="200" height="200"/>
+                            <p style={{backgroundColor:'green',width:'90%',margin:'auto'}}>REST and SOAP API integration. REST API development with Node.js and Loopback.</p>
                         </div>
                     </div>
                     <div class="w-100"></div>
                     <div class="col-sm">
-                        <div style={{ backgroundColor: 'red' }}>
+                        <div style={{ backgroundColor: 'red',textAlign:'center' }}>
                             <p>Database Design</p>
-                            <p>Database architectures in MongoDB, MySQL or Parse Server, always aiming for performance, scale and stability.</p>
+                            <img src={require('../../assets/database-development.png')} width="200" height="200"/>
+                            <p style={{backgroundColor:'green',width:'90%',margin:'auto'}}>Database architectures in MongoDB, MySQL or Parse Server, always aiming for performance, scale and stability.</p>
                         </div>
                     </div>
                     <div class="col-sm">
-                        <div style={{ backgroundColor: 'red' }}>
+                        <div style={{ backgroundColor: 'red',textAlign:'center' }}>
                             <p>Cloud Integration</p>
-                            <p>Deployment of NodeJS apps, ReactJS apps and databases to leading cloud platforms such as Heroku and Amazon AWS.</p>
+                            <img src={require('../../assets/cloud-development.png')} width="200" height="200"/>
+                            <p style={{backgroundColor:'green',width:'90%',margin:'auto'}}>Deployment of NodeJS apps, ReactJS apps and databases to leading cloud platforms such as Heroku and Amazon AWS.</p>
                         </div>
                     </div>
 
                     <div class="col-sm">
-                        <div style={{ backgroundColor: 'red' }}>
+                        <div style={{ backgroundColor: 'red',textAlign:'center' }}>
                             <p>Payment Gateway Integration</p>
-                            <p>Integration with the most popular Payment Gateways such as Stripe and Paypal.</p>
+                            <img src={require('../../assets/payment-development.png')} width="200" height="200"/>
+                            <p style={{backgroundColor:'green',width:'90%',margin:'auto'}}>Integration with the most popular Payment Gateways such as Stripe and Paypal.</p>
                         </div>
                     </div>
                 </div>
